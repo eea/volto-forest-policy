b742f1721f0b18298c95535bac1cb5425dbfe74f
dana-cfc4 <dana.comiselu4@gmail.com>
2023-02-24 17:51:04 +0200

    Fix main menu padding

--- src/components/theme/Header/HeaderNavigation.jsx
+++ src/components/theme/Header/HeaderNavigation.jsx
@@ -70,7 +70,6 @@ const MobileNav = ({ items, activeItem }) => {
 const HeaderNavigation = ({ items, pageWidth }) => {
   const [activeItem, setActiveItem] = React.useState('');
   const [isMobile, setIsMobile] = React.useState(false);
-  const [isTablet, setIsTablet] = React.useState(false);
   const [itemsIncrement, setItemsIncrement] = React.useState(0);
   const [itemsPerPage, setItemsPerPage] = React.useState(
     items && items.length < 4 ? items.length : 4,
@@ -109,16 +108,9 @@ const HeaderNavigation = ({ items, pageWidth }) => {
     }
     if (pageWidth && pageWidth <= 768) {
       setIsMobile(true);
-      setIsTablet(false);
     }
     if (pageWidth && pageWidth > 768) {
       setIsMobile(false);
-    }
-    if (pageWidth && pageWidth <= 1300 && pageWidth > 768) {
-      setIsTablet(true);
-    }
-    if (pageWidth && pageWidth > 1300) {
-      setIsTablet(false);
     }
     if (pageWidth && pageWidth > 1240) {
       if (items.length >= 6) {
