0bc9db889acbbd57f8e25a00b3ce02a8a17f272d
Tripon Eugen <tripon.eugen@gmail.com>
2025-05-06 08:59:57 +0300

    Refs #285599 - publication year histogram

--- src/search/fise/facets.js
+++ src/search/fise/facets.js
@@ -1,5 +1,7 @@
 import { objectProvides, issued_date } from '../common';
 
+import { makeRange } from '@eeacms/search';
+
 const countries = {
   field: 'country.keyword',
   factory: 'MultiTermFacet',
@@ -28,8 +30,51 @@ const updateFrequency = {
   alwaysVisible: true,
 };
 
+// const publicationYearForResource = {
+//   field: 'publicationYear.keyword',
+//   factory: 'MultiTermFacet',
+//   label: 'Publication Year',
+//   showInFacetsList: true,
+//   filterType: 'any',
+//   isFilterable: false,
+//   show: 10000,
+//   isMulti: true,
+//   // sortOn: 'custom',
+//   // sortOnCustomLabel: 'Alphabetical',
+//   alwaysVisible: true,
+// };
+const publicationYearHistogram = {
+  field: 'publicationYear.keyword',
+  factory: 'HistogramFacet',
+  label: 'Publication Year Histogram',
+  showInFacetsList: true,
+  filterType: 'any',
+  isFilterable: false,
+  show: 10000,
+  isMulti: true,
+  ranges: makeRange({
+    step: 1,
+    normalRange: [1950, 2500],
+    includeOutlierStart: false,
+    includeOutlierEnd: false,
+  }),
+  step: 10,
+  aggs_script: 'def vals = doc["publicationYear"]; return vals;',
+
+  // sortOn: 'custom',
+  // sortOnCustomLabel: 'Alphabetical',
+  alwaysVisible: true,
+};
+
 objectProvides['alwaysVisible'] = true;
 
-const facets = [objectProvides, countries, updateFrequency, issued_date];
+const facets = [
+  objectProvides,
+  countries,
+  updateFrequency,
+  // publicationYearForResource,
+  publicationYearHistogram,
+  issued_date,
+];
 
 export default facets;
