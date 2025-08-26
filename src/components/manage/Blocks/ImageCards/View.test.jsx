b58646e6c4dc6f287a231582d40e7dc6a9580e5c
Tripon Eugen <tripon.eugen@gmail.com>
2025-08-26 11:45:21 +0300

    Refs #288965 - fise search card try test

---
+++ src/components/manage/Blocks/ImageCards/View.test.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import { MemoryRouter } from 'react-router-dom';
+import configureStore from 'redux-mock-store';
+import { render } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+import { Provider } from 'react-intl-redux';
+import View from './View';
+
+const mockStore = configureStore();
+
+describe('View', () => {
+  it('should render the component', () => {
+    const data = {
+      attachedimage: '/my-test-image-href',
+    };
+
+    const store = mockStore({
+      userSession: { token: '1234' },
+      intl: {
+        locale: 'en',
+        messages: {},
+      },
+    });
+
+    const { container } = render(
+      <Provider store={store}>
+        <MemoryRouter>
+          <View {...data} />
+        </MemoryRouter>
+      </Provider>,
+    );
+    expect(container.querySelector('.cards-tile-image')).toBeInTheDocument();
+  });
+});
