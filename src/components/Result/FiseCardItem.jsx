59fdff99d6ed338f40d7870df32fa71ac288d869
Claudia Ifrim <ifrim.claudia@gmail.com>
2026-04-22 14:53:42 +0300

    fix es lint - remove unused DateTime

--- src/components/Result/FiseCardItem.jsx
+++ src/components/Result/FiseCardItem.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Label } from 'semantic-ui-react';
-import { StringList, DateTime } from '@eeacms/search/components';
+import { StringList } from '@eeacms/search/components';
 import { useAppConfig } from '@eeacms/search/lib/hocs';
 
 import ExternalLink from '@eeacms/search/components/Result/ExternalLink';
